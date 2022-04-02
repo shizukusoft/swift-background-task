eaa33a1d5b3772a18dc5e2ce97e3b2e823c203cb
Jaehong Kang <sinoru@me.com>
2022-04-03 00:50:22 +0900

    Enhances ExtendedBackgroundExecution

--- Sources/BackgroundTask/ExtendedBackgroundExecution.swift
+++ Sources/BackgroundTask/ExtendedBackgroundExecution.swift
@@ -6,7 +6,9 @@
 //
 
 import Foundation
+#if !os(macOS)
 import os
+#endif
 
 let moduleIdentifier = "com.tweetnest.BackgroundTask"
 
@@ -14,24 +16,24 @@ extension Task where Success == Never, Failure == Never {
     @TaskLocal fileprivate static var isInExtendedBackgroundExecution: Bool = false
 }
 
-@Sendable public func withExtendedBackgroundExecution<T>(identifier: String, body: @escaping () async throws -> T) async rethrows -> T {
+@Sendable public func withExtendedBackgroundExecution<T>(identifier: String, priority: TaskPriority? = nil, body: @escaping () async throws -> T) async rethrows -> T {
     guard Task.isInExtendedBackgroundExecution == false else {
         return try await body()
     }
 
     return try await Task.$isInExtendedBackgroundExecution.withValue(true) {
-        if #available(macOS 11.0, *) {
-            let logger = Logger(subsystem: moduleIdentifier, category: "extended-background-execution")
+        let expiringTask = ExpiringTask(priority: priority) {
+            try await body()
+        }
 
-            let expiringTask = ExpiringTask {
-                try await body()
-            }
-
+        return try await withTaskCancellationHandler {
             #if os(macOS)
             let token = ProcessInfo.processInfo.beginActivity(options: [.idleSystemSleepDisabled, .suddenTerminationDisabled, .automaticTerminationDisabled], reason: identifier)
             defer {
                 ProcessInfo.processInfo.endActivity(token)
             }
+
+            return try await expiringTask.value
             #else
             let dispatchGroup = DispatchGroup()
             dispatchGroup.enter()
@@ -39,64 +41,45 @@ extension Task where Success == Never, Failure == Never {
                 dispatchGroup.leave()
             }
 
-            ProcessInfo.processInfo.performExpiringActivity(withReason: identifier) { expired in
-                if expired {
-                    logger.notice("\(identifier, privacy: .public): Expiring activity expired")
-                    expiringTask.cancel()
-                    logger.notice("\(identifier, privacy: .public): Expiring activity expirationHandler finished")
-                } else {
-                    logger.info("\(identifier, privacy: .public): Start expiring activity")
-                    dispatchGroup.wait()
-                    logger.info("\(identifier, privacy: .public): Expiring activity finished")
+            if #available(iOS 14.0, watchOS 7.0, tvOS 14.0, *) {
+                let logger = Logger(subsystem: moduleIdentifier, category: "extended-background-execution")
+
+                ProcessInfo.processInfo.performExpiringActivity(withReason: identifier) { expired in
+                    if expired {
+                        logger.notice("\(identifier, privacy: .public): Expiring activity expired")
+                        expiringTask.cancel()
+                        logger.notice("\(identifier, privacy: .public): Expiring activity expirationHandler finished")
+                    } else {
+                        logger.info("\(identifier, privacy: .public): Start expiring activity")
+                        dispatchGroup.wait()
+                        logger.info("\(identifier, privacy: .public): Expiring activity finished")
+                    }
                 }
+
+                logger.notice("\(identifier, privacy: .public): Start")
+                defer {
+                    logger.notice("\(identifier, privacy: .public): Finished with cancelled: \(Task.isCancelled)")
+                }
+
+                return try await expiringTask.value
+            } else {
+                ProcessInfo.processInfo.performExpiringActivity(withReason: identifier) { expired in
+                    if expired {
+                        expiringTask.cancel()
+                    } else {
+                        dispatchGroup.wait()
+                    }
+                }
+
+                return try await expiringTask.value
             }
             #endif
-
-            logger.notice("\(identifier, privacy: .public): Start")
-            defer {
-                logger.notice("\(identifier, privacy: .public): Finished with cancelled: \(Task.isCancelled)")
-            }
-
-            return try await withTaskCancellationHandler {
-                try await expiringTask.value
-            } onCancel: {
-                expiringTask.cancel()
-            }
-        } else {
-            let expiringTask = ExpiringTask {
-                try await body()
-            }
-
-            #if os(macOS)
-            let token = ProcessInfo.processInfo.beginActivity(options: [.idleSystemSleepDisabled, .suddenTerminationDisabled, .automaticTerminationDisabled], reason: identifier)
-            defer {
-                ProcessInfo.processInfo.endActivity(token)
-            }
-            #else
-            let dispatchGroup = DispatchGroup()
-            dispatchGroup.enter()
-            defer {
-                dispatchGroup.leave()
-            }
-
-            ProcessInfo.processInfo.performExpiringActivity(withReason: identifier) { expired in
-                if expired {
-                    expiringTask.cancel()
-                } else {
-                    dispatchGroup.wait()
-                }
-            }
-            #endif
-
-            return try await withTaskCancellationHandler {
-                try await expiringTask.value
-            } onCancel: {
-                expiringTask.cancel()
-            }
+        } onCancel: {
+            expiringTask.cancel()
         }
     }
 }
 
-@Sendable @inlinable public func withExtendedBackgroundExecution<T>(function: String = #function, fileID: String = #fileID, line: Int = #line, body: @escaping () async throws -> T) async rethrows -> T {
-    try await withExtendedBackgroundExecution(identifier: "\(function) (\(fileID):\(line))", body: body)
+@Sendable @inlinable public func withExtendedBackgroundExecution<T>(function: String = #function, fileID: String = #fileID, line: Int = #line, priority: TaskPriority? = nil, body: @escaping () async throws -> T) async rethrows -> T {
+    try await withExtendedBackgroundExecution(identifier: "\(function) (\(fileID):\(line))", priority: priority, body: body)
 }
