c4848acc3d34aee5d094f041b568563a8cf153b0
Jaehong Kang <sinoru@me.com>
2022-04-21 19:14:37 +0900

    Adds sync withExtendedBackgroundExecution

--- Sources/BackgroundTask/ExtendedBackgroundExecution.swift
+++ Sources/BackgroundTask/ExtendedBackgroundExecution.swift
@@ -22,6 +22,48 @@ private func log(identifier: String, level: OSLogType, _ message: String) {
     }
 }
 
+@inlinable
+public func withExtendedBackgroundExecution<T>(function: String = #function, fileID: String = #fileID, line: Int = #line, expirationHandler: (@Sendable () -> Void)? = nil, body: () throws -> T) rethrows -> T {
+    try withExtendedBackgroundExecution(identifier: "\(function) (\(fileID):\(line))", expirationHandler: expirationHandler, body: body)
+}
+
+public func withExtendedBackgroundExecution<T>(identifier: String, expirationHandler: (@Sendable () -> Void)? = nil, body: () throws -> T) rethrows -> T {
+    guard Task.isInExtendedBackgroundExecution == false else {
+        return try body()
+    }
+
+    #if os(macOS)
+    let token = ProcessInfo.processInfo.beginActivity(options: [.idleSystemSleepDisabled, .suddenTerminationDisabled, .automaticTerminationDisabled], reason: identifier)
+    defer {
+        ProcessInfo.processInfo.endActivity(token)
+    }
+    #else
+    let dispatchSemaphore = DispatchSemaphore(value: 0)
+    defer {
+        dispatchSemaphore.signal()
+    }
+
+    ProcessInfo.processInfo.performExpiringActivity(withReason: identifier) { expired in
+        if expired {
+            log(identifier: identifier, level: .default, "Expiring activity expired")
+            expirationHandler?()
+            log(identifier: identifier, level: .default, "Expiring activity expirationHandler finished")
+        } else {
+            log(identifier: identifier, level: .info, "Start expiring activity")
+            dispatchSemaphore.wait()
+            log(identifier: identifier, level: .info, "Expiring activity finished")
+        }
+    }
+    #endif
+
+    log(identifier: identifier, level: .default, "Start")
+    defer {
+        log(identifier: identifier, level: .default, "Finished with cancelled: \(Task.isCancelled)")
+    }
+
+    return try body()
+}
+
 extension Task where Success == Never, Failure == Never {
     @TaskLocal fileprivate static var isInExtendedBackgroundExecution: Bool = false
 }
