2e14f88ac2783f093e76b357fc5a870422a947dc
Jaehong Kang <sinoru@me.com>
2022-05-01 21:25:39 +0900

    Fixes for wrong isInExtendedBackgroundExecution value

--- Sources/BackgroundTask/ExtendedBackgroundExecution+Task.swift
+++ Sources/BackgroundTask/ExtendedBackgroundExecution+Task.swift
@@ -19,61 +19,65 @@ public func withExtendedBackgroundExecution<T>(
         return try await body()
     }
 
+    #if os(macOS)
+    let token = ProcessInfo.processInfo.beginActivity(
+        options: [.idleSystemSleepDisabled, .suddenTerminationDisabled, .automaticTerminationDisabled],
+        reason: identifier
+    )
+    defer {
+        ProcessInfo.processInfo.endActivity(token)
+    }
+
     return try await ExtendedBackgroundExecution.$isInExtendedBackgroundExecution.withValue(true) {
-        #if os(macOS)
-        let token = ProcessInfo.processInfo.beginActivity(
-            options: [.idleSystemSleepDisabled, .suddenTerminationDisabled, .automaticTerminationDisabled],
-            reason: identifier
-        )
-        defer {
-            ProcessInfo.processInfo.endActivity(token)
-        }
+        try await body()
+    }
+    #elseif os(iOS) || os(watchOS) || os(tvOS)
+    let taskPriority = Task.currentPriority
 
-        return try await body()
-        #elseif os(iOS) || os(watchOS) || os(tvOS)
-        let taskPriority = Task.currentPriority
+    let task: Task<T, Error> = try await withCheckedThrowingContinuation { continuation in
+        var currentTask: Task<T, Error>?
 
-        let task: Task<T, Error> = try await withCheckedThrowingContinuation { continuation in
-            var currentTask: Task<T, Error>?
-
-            ProcessInfo.processInfo.performExpiringActivity(withReason: identifier) { expired in
-                switch (currentTask, expired) {
-                case (nil, true):
-                    continuation.resume(throwing: TaskAssertionError())
-                case (nil, false):
-                    ExtendedBackgroundExecution.log(level: .info, identifier: identifier, "Start expiring activity")
-                    let task = Task<T, Error>(priority: taskPriority) {
-                        ExtendedBackgroundExecution.log(level: .default, identifier: identifier, "Start")
-                        defer {
-                            ExtendedBackgroundExecution.log(level: .default, identifier: identifier, "Finished with cancelled: \(Task.isCancelled)")
-                        }
-
-                        return try await body()
+        ProcessInfo.processInfo.performExpiringActivity(withReason: identifier) { expired in
+            switch (currentTask, expired) {
+            case (nil, true):
+                continuation.resume(throwing: TaskAssertionError())
+            case (nil, false):
+                ExtendedBackgroundExecution.log(level: .info, identifier: identifier, "Start expiring activity")
+                let task = Task<T, Error>(priority: taskPriority) {
+                    ExtendedBackgroundExecution.log(level: .default, identifier: identifier, "Start")
+                    defer {
+                        ExtendedBackgroundExecution.log(level: .default, identifier: identifier, "Finished with cancelled: \(Task.isCancelled)")
                     }
-                    currentTask = task
-                    continuation.resume(returning: task)
-                    task.waitUntilFinished()
-                    ExtendedBackgroundExecution.log(level: .info, identifier: identifier, "Expiring activity finished")
-                case (.some(let currentTask), true):
-                    ExtendedBackgroundExecution.log(level: .default, identifier: identifier, "Expiring activity expired")
-                    currentTask.cancel()
-                    currentTask.waitUntilFinished()
-                    ExtendedBackgroundExecution.log(level: .default, identifier: identifier, "Expiring activity expirationHandler finished")
-                default:
-                    fatalError()
+
+                    return try await ExtendedBackgroundExecution.$isInExtendedBackgroundExecution.withValue(true) {
+                        try await body()
+                    }
                 }
+                currentTask = task
+                continuation.resume(returning: task)
+                task.waitUntilFinished()
+                ExtendedBackgroundExecution.log(level: .info, identifier: identifier, "Expiring activity finished")
+            case (.some(let currentTask), true):
+                ExtendedBackgroundExecution.log(level: .default, identifier: identifier, "Expiring activity expired")
+                currentTask.cancel()
+                currentTask.waitUntilFinished()
+                ExtendedBackgroundExecution.log(level: .default, identifier: identifier, "Expiring activity expirationHandler finished")
+            default:
+                fatalError()
             }
         }
-
-        return try await withTaskCancellationHandler {
-            try await task.value
-        } onCancel: {
-            task.cancel()
-        }
-        #else
-        return try await body()
-        #endif
     }
+
+    return try await withTaskCancellationHandler {
+        try await task.value
+    } onCancel: {
+        task.cancel()
+    }
+    #else
+    return try await ExtendedBackgroundExecution.$isInExtendedBackgroundExecution.withValue(true) {
+        try await body()
+    }
+    #endif
 }
 
 @Sendable @inlinable
